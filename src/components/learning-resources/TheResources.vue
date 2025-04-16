7085b0a652dc5cf57cee6d3468986a523266a98e
SahakKDev <sahak.keshishyan.dev@gmail.com>
2025-04-16 10:29:34 +0400

    conditionally render AddResources and StoredResources, provide data via provider

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,61 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+
+  <component :is="selectedTab" />
+</template>
+
+<script>
+import AddResource from './AddResource.vue'
+import StoredResources from './StoredResources.vue'
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resouces',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    }
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    }
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab
+    },
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat'
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat'
+    },
+  },
+}
+</script>
